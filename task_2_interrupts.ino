e717f59d2d118a07cb6d7d6b48a5c54663aa6544
lucycodesss <93416966+lucycodesss@users.noreply.github.com>
2023-02-22 02:20:21 +0530

    Add files via upload

---
+++ task_2_interrupts.ino
@@ -0,0 +1,24 @@
+int motionSens = 7;
+bool curr_State = false;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(motionSens, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(motionSens), swap, CHANGE);
+}
+
+void loop()
+{
+  swap();
+  delay(1000);
+}
+
+void swap()
+{
+  curr_State = !curr_State;
+  digitalWrite(LED_BUILTIN, curr_State);
+  Serial.println("Interrupted");
+}
